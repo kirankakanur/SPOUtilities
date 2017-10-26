98959752d67d1d614f692941c9355c2e11a0fb2a
Kiran Kakanur <kkakanur@rightpoint.com>
2017-10-26 08:39:33 -0500

    Initial commit

---
+++ SPOUtilities/Program.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security;
+using System.Configuration;
+using System.IO;
+using Microsoft.SharePoint.Client;
+
+namespace SPOUtilities
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string siteUrl = ConfigurationManager.AppSettings["SPOSiteUrl"];
+            string userName = ConfigurationManager.AppSettings["SPOUserName"];
+            string userPassword = ConfigurationManager.AppSettings["SPOPassword"];
+            string csvFilePath = ConfigurationManager.AppSettings["CSVFilePath"];
+            DisplayJobRunOptions(siteUrl, userName, userPassword, csvFilePath);
+
+            Console.WriteLine("Done processing. Click Enter to close this window.");
+            Console.ReadLine();
+        }
+
+        public static void DisplayJobRunOptions(string siteUrl, string userName, string password, string csvFilePath)
+        {
+            string options = "";
+            string RunMode = "";
+            
+            Console.WriteLine();
+            Console.WriteLine("What would you like to do?");
+            if (string.IsNullOrEmpty(RunMode))
+            {
+                Console.WriteLine("1 - Enable Major and Minor Versions in Document Library");
+                Console.WriteLine("2 - Delete Old Document Versions in Document Library");
+                Console.WriteLine("3 - Get Last Modified Information from Site and Document Library");
+                Console.WriteLine("");
+                Console.WriteLine("Enter a number from the above list and then click Enter:");
+                RunMode = Console.ReadLine();
+
+                options = RunMode.GetRightHalf("-").Trim();
+                RunMode = RunMode.GetLeftHalf("-").Trim();
+            }
+
+            if (!String.IsNullOrEmpty(RunMode))
+            {
+                switch (RunMode)
+                {
+                    case "1":
+                        EnableVersioningInDocLib(siteUrl, userName, password, csvFilePath);
+                        break;
+
+                    case "2":
+                        DeleteOldDocumentVersions(siteUrl, userName, password, csvFilePath);
+                        break;
+
+                    case "3":
+                        GetLastModifiedInfo(siteUrl, userName, password, csvFilePath);
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please select an option and try again.");
+                Console.ReadLine();
+            }
+
+        }
+
+
+        static ClientContext GetUserContext(string siteUrl, string userName, string password)
+        {
+            var securePassword = new SecureString();
+            foreach (char c in password)
+            {
+                securePassword.AppendChar(c);
+            }
+
+            var spoCredentials = new SharePointOnlineCredentials(userName, securePassword);
+            var spoContext = new ClientContext(siteUrl);
+
+            spoContext.Credentials = spoCredentials;
+            return spoContext;
+
+        }
+
+        static void EnableVersioningInDocLib(string siteUrl, string userName, string userPassword, string csvFilePath)
+        {
+            try
+            {
+                // get client context
+                ClientContext context = GetUserContext(siteUrl, userName, userPassword);
+
+                // get web
+                Web web = context.Web;
+                context.Load(web);
+                context.ExecuteQuery();
+
+                // get list
+                List documentslist = web.Lists.GetByTitle("Documents");
+                context.Load(documentslist);
+                context.ExecuteQuery();
+
+                if (documentslist.EnableVersioning && !documentslist.EnableMinorVersions)
+                {
+                    documentslist.EnableMinorVersions = true;
+                    documentslist.MajorVersionLimit = 5;
+                    documentslist.MajorWithMinorVersionsLimit = 2;
+
+                    documentslist.Update();
+                    context.ExecuteQuery();
+                }
+            }
+
+            catch (ClientRequestException clientEx)
+            {
+                Console.WriteLine("Client side error occurred: {0} \n{1} " + clientEx.Message + clientEx.InnerException);
+                throw clientEx;
+            }
+
+            catch (ServerException serverEx)
+            {
+                Console.WriteLine("Server side error occurred: {0} \n{1} " + serverEx.Message + serverEx.InnerException);
+                throw serverEx;
+            }
+
+        }
+
+        static void DeleteOldDocumentVersions(string siteUrl, string userName, string userPassword, string csvFilePath)
+        {
+
+            int maxDocumentVersions = 15;//this is the maximum number of document versions that will be stored in the document library
+
+            //get client context
+            ClientContext context = GetUserContext(siteUrl, userName, userPassword);
+
+            //get web
+            Web web = context.Site.RootWeb;
+            context.Load(web, w => w.Webs); //include subwebs
+            context.ExecuteQuery();
+
+            //caml query to get all documents in the library
+            CamlQuery camlQuery = new CamlQuery();
+            camlQuery.ViewXml = "<View Scope='RecursiveAll'></View>";
+
+            //create StringBuilder object for storing documents info
+            StringBuilder docsCSVFile = new StringBuilder();
+            docsCSVFile.AppendLine("Title, URL, Versions");
+
+            // Loop through all the webs  
+            foreach (Web subWeb in web.Webs)
+            {
+                context.Load(subWeb, subw => subw.Webs); //include title and all webs
+                context.ExecuteQuery();
+
+                Console.WriteLine("Processing site -> {0}", subWeb.Url);
+
+                // Loop through all webs in subwebs
+                foreach (Web subsubWeb in subWeb.Webs)
+                {
+                    context.Load(subsubWeb, subsubw => subsubw.Title); //include title
+                    context.ExecuteQuery();
+
+                    Console.WriteLine("Processing site -> {0}", subsubWeb.Url);
+
+                    //get list
+                    List list = subsubWeb.Lists.GetByTitle("Documents");
+                    context.Load(list);
+                    context.ExecuteQuery();
+
+
+                    //get all documents in the list
+                    ListItemCollection collListItem = list.GetItems(camlQuery);
+
+                    context.Load(collListItem,
+                    items => items.Include(
+                       item => item.Id,
+                       item => item["Title"],
+                       item => item["FileRef"]));
+
+                    context.ExecuteQuery();
+
+                    if (collListItem.Count > 0)
+                    {
+                        foreach (ListItem oListItem in collListItem)
+                        {
+                            //get list item
+                            context.Load(oListItem, listItem => listItem.File, listItem => listItem.FileSystemObjectType);
+                            context.ExecuteQuery();
+
+                            //get file associated with list item
+                            Microsoft.SharePoint.Client.File file = oListItem.File;
+                            context.Load(file, f => f.CheckOutType, f => f.Versions);
+                            context.ExecuteQuery();
+
+                            if (oListItem.FileSystemObjectType == FileSystemObjectType.File) //check if it is a File. 
+                            {
+                                if (file.CheckOutType == CheckOutType.None) //only process the file if it is not checked out
+                                {
+                                    //get file version collection
+                                    FileVersionCollection fvCollection = file.Versions;
+                                    context.Load(fvCollection);
+                                    context.ExecuteQuery();
+                                    Console.WriteLine("Versions count for file {0} is {1} ", file.Name, file.Versions.Count.ToString());
+
+                                    //check if current number of versions is greater than the max number of versions
+                                    if (file.Versions.Count + 1 > maxDocumentVersions) //add 1 to versions count, since it is zero index based
+                                    {
+                                        Console.WriteLine("Processing document titled, {0}, with URL {1} and {2} versions", oListItem["Title"], oListItem["FileRef"], (file.Versions.Count + 1).ToString());
+                                        docsCSVFile.AppendLine(oListItem["Title"] + "," + oListItem["FileRef"] + "," + (file.Versions.Count + 1).ToString());
+                                        int versionsToDelete = (file.Versions.Count + 1) - maxDocumentVersions; //add 1 to versions count, since it is zero index based
+
+                                        //loop through collection and delete old versions
+                                        for (int i = 0; i < versionsToDelete; i++)
+                                        {
+                                            FileVersion fileVersion = fvCollection[0];//always delete the first (i.e. oldest) verion
+                                            Console.WriteLine("fileVersion with label {0} created on {1} {2} will be deleted", fileVersion.VersionLabel, fileVersion.Created.ToShortDateString(), fileVersion.Created.ToShortTimeString());
+                                            fileVersion.DeleteObject();
+                                        }
+                                        oListItem.SystemUpdate();
+                                        context.ExecuteQuery();
+                                    }//check for number of document versions
+                                }//check for file checkout
+                            }//check if it is a file and not a folder
+                        }//loop through documents 
+                    }//check if library has documents
+                }//loop through subWeb.Webs
+            }//loop through web.webs
+
+            //Write docs info to CSV file
+            System.IO.File.AppendAllText(csvFilePath, docsCSVFile.ToString());
+        }
+
+        static void GetLastModifiedInfo(string siteUrl, string userName, string userPassword, string csvFilePath)
+        {
+
+            //get client context
+            ClientContext context = GetUserContext(siteUrl, userName, userPassword);
+
+            //get web
+            Web web = context.Site.RootWeb;
+            context.Load(web, w => w.Webs); //include subwebs
+            context.ExecuteQuery();
+
+            //create StringBuilder object for storing documents info
+            StringBuilder lastModCSVFile = new StringBuilder();
+            lastModCSVFile.AppendLine("Title, URL, Site-LastItemModifiedDate, Site-LastItemUserModifiedDate, Lib-LastItemDeletedDate, Lib-LastItemModifiedDate, Lib-LastItemUserModifiedDate");
+
+            // Loop through all the webs  
+            foreach (Web subWeb in web.Webs)
+            {
+                context.Load(subWeb, subw => subw.Webs); //include title and all webs
+                context.ExecuteQuery();
+
+
+                // Loop through all webs in subwebs
+                foreach (Web subsubWeb in subWeb.Webs)
+                {
+                    context.Load(subsubWeb, subsubw => subsubw.Title, subsubw => subsubw.LastItemModifiedDate, subsubw => subsubw.LastItemUserModifiedDate); //include title, lastitemmodifieddate, lastitemusermodifieddate
+                    context.ExecuteQuery();
+
+                    Console.WriteLine("Processing site -> {0}", subsubWeb.Url);
+
+                    //get list
+                    List list = subsubWeb.Lists.GetByTitle("Documents");
+                    context.Load(list, l => l.LastItemDeletedDate, l => l.LastItemModifiedDate, l => l.LastItemUserModifiedDate); //include lastitemdeleteddate, lastitemmodifieddate, lastitemusermodifieddate
+                    context.ExecuteQuery();
+
+                    //store info in CSV file
+                    lastModCSVFile.AppendLine(subsubWeb.Title + "," +
+                                                         subsubWeb.Url + "," +
+                                                         subsubWeb.LastItemModifiedDate.ToShortDateString() + " " +
+                                                         subsubWeb.LastItemModifiedDate.ToShortTimeString() + "," +
+                                                         subsubWeb.LastItemUserModifiedDate.ToShortDateString() + " " +
+                                                         subsubWeb.LastItemUserModifiedDate.ToShortTimeString() + "," +
+                                                         list.LastItemDeletedDate.ToShortDateString() + " " +
+                                                         list.LastItemDeletedDate.ToShortTimeString() + "," +
+                                                         list.LastItemModifiedDate.ToShortDateString() + " " +
+                                                         list.LastItemModifiedDate.ToShortTimeString() + "," +
+                                                         list.LastItemUserModifiedDate.ToShortDateString() + " " +
+                                                         list.LastItemUserModifiedDate.ToShortTimeString());
+                }//loop through subWeb.Webs
+            }//loop through web.webs
+
+            //Write docs info to CSV file
+            System.IO.File.AppendAllText(csvFilePath, lastModCSVFile.ToString());
+        }
+    }
+}
---
+++ SPOUtilities/StringExtensions.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPOUtilities
+{
+    using System.Threading;
+
+    /// <summary>
+    /// String extension methods
+    /// </summary>
+
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Strips the img source.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns></returns>
+        public static string StripImgSrc(this string value)
+        {
+            if (String.IsNullOrEmpty(value)) return String.Empty;
+
+            if (value.ToLowerInvariant().StartsWith("<img"))
+            {
+                var start = value.IndexOf("src=") + 5;
+                if (start == -1) return String.Empty;
+
+                var len = value.IndexOf("\"", start) - start;
+
+                if (len == -1) len = value.IndexOf("'", start) - start;
+
+                if (len > -1) return value.Substring(start, len);
+            }
+
+            return value;
+        }
+
+        /// <summary>
+        /// To title case.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns></returns>
+        public static string ToTitleCase(this string value)
+        {
+            return Thread.CurrentThread.CurrentCulture.TextInfo.ToTitleCase(value.ToLower());
+        }
+
+        /// <summary>
+        /// Makes the query string friendly.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns></returns>
+        public static string MakeQueryStringFriendly(this string value)
+        {
+            return System.Web.HttpUtility.UrlEncode(value);
+        }
+
+        /// <summary>
+        /// Truncates to last character.
+        /// </summary>
+        /// <param name="valueToTruncate">The value to truncate.</param>
+        /// <param name="maxLength">The maximum length.</param>
+        /// <param name="addElipsis">if set to <c>true</c> [add elipsis].</param>
+        /// <returns></returns>
+        public static string TruncateToLastCharacter(this string valueToTruncate, int maxLength, bool addElipsis)
+        {
+            //make room for ellipsis
+            if (addElipsis && maxLength > 3)
+                maxLength = maxLength - 3;
+
+            if (valueToTruncate == null)
+            {
+                return String.Empty;
+            }
+
+            if (valueToTruncate.Length <= maxLength)
+            {
+                return valueToTruncate;
+            }
+            string retValue = valueToTruncate.Remove(maxLength);
+
+            if (addElipsis)
+            {
+                retValue += "&hellip;";
+            }
+            return retValue;
+        }
+
+        /// <summary>
+        /// Truncates to last space.
+        /// </summary>
+        /// <param name="valueToTruncate">The value to truncate.</param>
+        /// <param name="maxLength">The maximum length.</param>
+        /// <param name="addElipsis">if set to <c>true</c> [add elipsis].</param>
+        /// <returns></returns>
+        public static string TruncateToLastSpace(this string valueToTruncate, int maxLength, bool addElipsis)
+        {
+            if (valueToTruncate == null)
+            {
+                return String.Empty;
+            }
+
+            if (valueToTruncate.Length <= maxLength)
+            {
+                return valueToTruncate;
+            }
+
+            string retValue = valueToTruncate;
+
+            int lastSpaceIndex = retValue.LastIndexOf(" ",
+                                                      maxLength, StringComparison.CurrentCultureIgnoreCase);
+
+            if (lastSpaceIndex > -1)
+            {
+                retValue = retValue.Remove(lastSpaceIndex);
+            }
+
+            if (retValue.Length < valueToTruncate.Length && addElipsis)
+            {
+                retValue += "&hellip;";
+            }
+            return retValue;
+        }
+
+        /// <summary>
+        /// Truncates to last space.
+        /// </summary>
+        /// <param name="valueToTruncate">The value to truncate.</param>
+        /// <param name="maxLength">The maximum length.</param>
+        /// <returns></returns>
+        public static string TruncateToLastSpace(this string valueToTruncate, int maxLength)
+        {
+            return TruncateToLastSpace(valueToTruncate, maxLength, true);
+        }
+
+        /// <summary>
+        /// Removes all HTML tags using HTML AgilityPack
+        /// </summary>
+        /// <param name="html">The HTML.</param>
+        /// <returns></returns>
+        //public static string RemoveAllHtmlTags(this string html)
+        //{
+        //    var doc = new HtmlDocument();
+        //    doc.LoadHtml(html);
+
+        //    return doc.DocumentNode.InnerText;
+        //}
+
+        /// <summary>
+        /// This method removes ALL style and class attributes from all tags in a string of Html.
+        /// </summary>
+        /// <param name="html"></param>
+        /// <returns></returns>
+        //public static string RemoveStyleAndClassAttributesFromHtml(this string html)
+        //{
+        //    if (html.StartsWith("<img"))
+        //    {
+        //        if (!html.EndsWith("</img>") && !html.EndsWith("/>"))
+        //            html = html.TrimEnd('>');
+        //        html += "/>";
+        //    }
+        //    html = html.Replace("&", "&amp;");
+        //    always add root node as html blocks don't always have root.
+        //    var preCleaned = html.Insert(0, "<rootnode>");
+        //    preCleaned += "</rootnode>";
+
+        //    var xmlDoc = XDocument.Parse(preCleaned);
+
+        //    xmlDoc.Descendants().Attributes("style").Remove();
+        //    xmlDoc.Descendants().Attributes("class").Remove();
+
+        //    var cleanXML = xmlDoc.ToString();
+
+        //    var output = cleanXML.Replace("<rootnode>", string.Empty).Replace("</rootnode>", string.Empty);
+
+        //    return output.Replace("&amp;", "&");
+        //}
+
+        /// <summary>
+        /// Gets the left half.
+        /// </summary>
+        /// <param name="str">The string.</param>
+        /// <param name="splitter">The splitter.</param>
+        /// <returns></returns>
+        public static string GetLeftHalf(this string str, char splitter)
+        {
+            string[] parts = str.Split(splitter);
+            return parts[0];
+
+        }
+
+        /// <summary>
+        /// Gets the left half.
+        /// </summary>
+        /// <param name="str">The string.</param>
+        /// <param name="splitter">The splitter.</param>
+        /// <returns></returns>
+        public static string GetLeftHalf(this string str, string splitter)
+        {
+            var parts = str.Split(new string[] { splitter }, StringSplitOptions.RemoveEmptyEntries);
+
+            return parts[0];
+        }
+
+        /// <summary>
+        /// Gets the right half.
+        /// </summary>
+        /// <param name="str">The string.</param>
+        /// <param name="splitter">The splitter.</param>
+        /// <returns></returns>
+        public static string GetRightHalf(this string str, char splitter)
+        {
+            string[] parts = str.Split(splitter);
+            int count = parts.Count();
+            if (count > 1)
+                return parts[count - 1];
+            return str;
+        }
+
+        /// <summary>
+        /// Gets the right half.
+        /// </summary>
+        /// <param name="str">The string.</param>
+        /// <param name="splitter">The splitter.</param>
+        /// <returns></returns>
+        public static string GetRightHalf(this string str, string splitter)
+        {
+            string[] parts = str.Split(new[] { splitter }, StringSplitOptions.RemoveEmptyEntries);
+            if (parts.Count() > 1)
+                return parts[parts.Count() - 1];
+            return str;
+        }
+
+        /// <summary>
+        /// Splits the and remove empty entries.
+        /// </summary>
+        /// <param name="str">The string.</param>
+        /// <param name="splitter">The splitter.</param>
+        /// <returns></returns>
+        public static string[] SplitAndRemoveEmptyEntries(this string str, string splitter)
+        {
+            return str.Split(splitter.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        /// <summary>
+        /// To safe string.
+        /// </summary>
+        /// <param name="obj">The object.</param>
+        /// <returns></returns>
+        public static string ToSafeString(this object obj)
+        {
+            return (obj ?? String.Empty).ToString();
+        }
+
+        /// <summary>
+        /// Deserializes a JSON object into list of strings, removing any blanks
+        /// </summary>
+        /// <param name="audiences">A serialized list of strings in JSON format</param>
+        /// <returns>List of strings</returns>
+        //public static IList<string> DeserializeStringListNoBlanks(this string audiences)
+        //{
+        //    var deserializedFilters = new JavaScriptSerializer().Deserialize<List<string>>(audiences);
+        //    deserializedFilters = deserializedFilters.Where(i => !string.IsNullOrEmpty(i)).ToList();
+        //    return deserializedFilters;
+        //}
+    }
+}
